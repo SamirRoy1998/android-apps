26956bbdbc502be1e32a55b51fe737e272c82e54
Samir <dev.sam693@gmail.com>
2024-04-23 13:22:10 +0530

    just added

---
+++ Test1/app/src/main/java/com/example/test1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.test1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    public void like(View view){
+        Toast.makeText(this, "Thank You", Toast.LENGTH_SHORT).show();
+    }
+    public void comments(View view){
+        Toast.makeText(this, "Write a Comment", Toast.LENGTH_SHORT).show();
+    }
+    public void share(View view){
+        Toast.makeText(this, "Loading Please Wait.....", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
