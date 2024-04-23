a33b07f7a889d830aea43c0a47aa05cbc6ae7a81
Samir <dev.sam693@gmail.com>
2024-04-23 20:21:46 +0530

    logic added

--- BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.bmicalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,40 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText editWeight, editHeightFt,editHeightInch;
+        Button btnCalculate;
+        TextView txt_result;
+
+        editWeight = findViewById(R.id.editWeight);
+        editHeightFt = findViewById(R.id.editHeightFt);
+        editHeightInch = findViewById(R.id.editHeightInch);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        txt_result = findViewById(R.id.txt_result);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               int weight = Integer.parseInt(editWeight.getText().toString());
+               int feet = Integer.parseInt(editHeightFt.getText().toString());
+               int inch = Integer.parseInt(editHeightInch.getText().toString());
+
+               int totalIn = feet * 12 + inch;
+               double totalCm = totalIn * 2.53;
+               double totalMtr = totalCm / 100;
+               double bmi = weight / (totalMtr * totalMtr);
+
+               if (bmi>25){
+                   txt_result.setText("You're Overweight");
+               }
+               else if (bmi<18){
+                   txt_result.setText("You're Underweight");
+               }
+               else {
+                   txt_result.setText("You're Healthy");
+               }
+            }
         });
     }
 }
