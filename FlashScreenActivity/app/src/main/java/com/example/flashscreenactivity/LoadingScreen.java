00154fd21b4f159045c842c12be6f1f978ad7828
Samir <dev.sam693@gmail.com>
2024-04-25 03:33:43 +0530

    just added

---
+++ FlashScreenActivity/app/src/main/java/com/example/flashscreenactivity/LoadingScreen.java
@@ -0,0 +1,36 @@
+package com.example.flashscreenactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_loading_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.LoadingScreen), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent IHome = new Intent(LoadingScreen.this, MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(IHome);
+                finish();
+            }
+        },4000);
+    }
+}
