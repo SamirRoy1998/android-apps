39df409c1db9a1183837268a23e5c42c4f8f3021
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-26 01:55:37 +0530

    Text View removed but dice images add and logic added

--- DiceRoller/app/src/main/java/com/example/diceRoller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceRoller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceRoller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,16 +20,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnRoll: Button= findViewById(R.id.Btn_Roll)
+        val btnRoll: Button = findViewById(R.id.Btn_Roll)
         btnRoll.setOnClickListener {
-//            Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
             diceRoll()
         }
     }
 
     private fun diceRoll() {
-        val text:TextView= findViewById(R.id.Txt_Roll)
-        val randomNumber= Random.nextInt(6)+1
-        text.text = randomNumber.toString()
+        val diceImage: ImageView = findViewById(R.id.ImgVw_Dice)
+        val randomNumber = Random.nextInt(6) + 1
+        val result = when (randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(result)
+        Toast.makeText(this,randomNumber.toString(),Toast.LENGTH_SHORT).show()
     }
 }
