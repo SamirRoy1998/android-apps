477219ba35d31b9237ff959b6bdd79f6ddb91c26
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-25 23:54:38 +0530

    Button click Toast added

--- DiceRoller/app/src/main/java/com/example/diceRoller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceRoller/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.diceRoller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnRoll: Button= findViewById(R.id.Btn_Roll)
+        btnRoll.setOnClickListener {
+            Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
         }
     }
 }
