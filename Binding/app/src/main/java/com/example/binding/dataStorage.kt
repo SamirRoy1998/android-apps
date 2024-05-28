ec448dc6e743b25a9b2d17601e8d5fd9b9fcf3b0
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-28 23:09:13 +0530

    data class unused variable removed

--- Binding/app/src/main/java/com/example/binding/dataStorage.kt
+++ Binding/app/src/main/java/com/example/binding/dataStorage.kt
@@ -4,11 +4,4 @@ class Name(var nameHeading: String = "", var userName: String = "")
 class Age(var ageHeading: String = "", var userAge: String = "")
 class Address(var addressHeading: String = "", var userAddress: String = "")
 class PhoneNumber(var phoneNumberHeading: String = "", var userPhoneNumber: String = "")
-class Text(
-    var heading: String = "",
-    var buttonName: String = "",
-    var editNameHint: String = "",
-    var editAgeHint: String = "",
-    var editAddressHint: String = "",
-    var editNumberHint: String = ""
-)
+class Text(var heading: String = "", var buttonName: String = "")
