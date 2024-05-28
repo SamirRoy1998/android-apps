e13e042192569ed3529d992100536f484ad9793b
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-28 22:23:34 +0530

    added text class for more data field

--- Binding/app/src/main/java/com/example/binding/dataStorage.kt
+++ Binding/app/src/main/java/com/example/binding/dataStorage.kt
@@ -4,3 +4,11 @@ class Name(var nameHeading: String = "", var userName: String = "")
 class Age(var ageHeading: String = "", var userAge: String = "")
 class Address(var addressHeading: String = "", var userAddress: String = "")
 class PhoneNumber(var phoneNumberHeading: String = "", var userPhoneNumber: String = "")
+class Text(
+    var heading: String = "",
+    var buttonName: String = "",
+    var editNameHint: String = "",
+    var editAgeHint: String = "",
+    var editAddressHint: String = "Enter your address",
+    var editNumberHint: String = ""
+)
