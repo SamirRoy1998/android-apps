457b4641af45b52f5a330f264381dede837470fe
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-28 20:59:23 +0530

    data Storage added in Kotlin class

---
+++ Binding/app/src/main/java/com/example/binding/dataStorage.kt
@@ -0,0 +1,6 @@
+package com.example.binding
+
+class Name(var nameHeading: String = "", var userName: String = "")
+class Age(var ageHeading: String = "", var userAge: String = "")
+class Address(var addressHeading: String = "", var userAddress: String = "")
+class PhoneNumber(var phoneNumberHeading: String = "", var userPhoneNumber: String = "")
