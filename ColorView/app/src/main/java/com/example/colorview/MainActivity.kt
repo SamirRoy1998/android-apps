d5987f9f1b543816bc1e08ad76ee7fadb82197b7
SamirRoy1998 <dev.sam693@gmail.com>
2024-05-29 19:20:38 +0530

    simple logic added

--- ColorView/app/src/main/java/com/example/colorview/MainActivity.kt
+++ ColorView/app/src/main/java/com/example/colorview/MainActivity.kt
@@ -1,20 +1,75 @@
 package com.example.colorview
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.colorview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var f = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        if (f) {
+            setContentView(R.layout.activity_main)
+        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setListeners()
+        binding.btn1.setOnClickListener {
+            binding.txtBox1.setBackgroundColor(Color.YELLOW)
+        }
+        binding.btn2.setOnClickListener {
+            binding.txtBox2.setBackgroundColor(Color.GREEN)
+        }
+        binding.btn3.setOnClickListener {
+            binding.txtBox3.setBackgroundColor(Color.RED)
+        }
+        binding.btn4.setOnClickListener {
+            binding.txtBox4.setBackgroundColor(Color.BLUE)
+        }
+        binding.btn5.setOnClickListener {
+            binding.txtBox5.setBackgroundColor(Color.MAGENTA)
+        }
+    }
+
+    private fun setListeners() {
+        val clickableView: List<View> = listOf(
+            binding.txtBox1,
+            binding.txtBox2,
+            binding.txtBox3,
+            binding.txtBox4,
+            binding.txtBox5,
+            binding.main
+        )
+        for (item in clickableView) {
+            item.setOnClickListener {
+                makeColor(it)
+            }
+        }
+    }
+
+    private fun makeColor(view: View) {
+        when (view.id) {
+            R.id.txtBox1 -> view.setBackgroundColor(Color.GRAY)
+            R.id.txtBox2 -> view.setBackgroundColor(Color.BLUE)
+            R.id.txtBox3 -> view.setBackgroundColor(Color.CYAN)
+            R.id.txtBox4 -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.txtBox5 -> view.setBackgroundColor(Color.GREEN)
+            R.id.main -> view.setBackgroundColor(Color.DKGRAY)
         }
     }
 }
