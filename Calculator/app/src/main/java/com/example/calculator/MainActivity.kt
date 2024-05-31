f777af7777f3a1b6a914e78f34ea124a0be8aca1
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-01 02:23:17 +0530

    clear button function added

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -28,5 +29,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btnClear.setOnClickListener {
+            clearResult()
+        }
+    }
+
+    private fun clearResult() {
+        val result = "Result: "
+        binding.txtResult.text =result
+        Toast.makeText(this,"Result Cleared",Toast.LENGTH_SHORT).show()
     }
 }
