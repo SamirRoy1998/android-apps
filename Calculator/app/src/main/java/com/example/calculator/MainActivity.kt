b0639aee0ddd607cf2ea857fc778d2a340330803
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-01 03:02:38 +0530

    multiplication function added

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -43,6 +43,10 @@ class MainActivity : AppCompatActivity() {
         binding.btnSubtraction.setOnClickListener {
             subtraction()
         }
+
+        binding.btnMultiplication.setOnClickListener {
+            multiplication()
+        }
     }
 
     private fun clearResult() {
@@ -83,6 +87,25 @@ class MainActivity : AppCompatActivity() {
             val x = binding.etInputX.text.toString()
             val y = binding.etInputY.text.toString()
             val z = x.toDouble() - y.toDouble()
+            binding.txtResult.text = "Result : $z"
+        } catch (e: Exception) {
+            Toast.makeText(
+                this,
+                "Please fill-up the X and Y both inputs\nto get result",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun multiplication() {
+        val key = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        key.hideSoftInputFromWindow(binding.etInputX.windowToken, 0)
+        key.hideSoftInputFromWindow(binding.etInputY.windowToken, 0)
+
+        try {
+            val x = binding.etInputX.text.toString()
+            val y = binding.etInputY.text.toString()
+            val z = x.toDouble() * y.toDouble()
             binding.txtResult.text = "Result : $z"
         } catch (e: Exception) {
             Toast.makeText(
