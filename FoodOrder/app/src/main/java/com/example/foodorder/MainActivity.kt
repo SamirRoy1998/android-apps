6814aca2057268d78ffa74a7ddf1147c5b26207b
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-01 21:13:16 +0530

    veg and Non-Veg visibily function set

--- FoodOrder/app/src/main/java/com/example/foodorder/MainActivity.kt
+++ FoodOrder/app/src/main/java/com/example/foodorder/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.example.foodorder
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.foodorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val f = false
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        if (f) {
+            setContentView(R.layout.activity_main)
+        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btnOrder.setOnClickListener {
+            Toast.makeText(this, "App is running", Toast.LENGTH_SHORT).show()
+        }
+
+        hideNonVeg()
+
+        binding.rbR1Veg.setOnClickListener {
+            binding.rbR1Veg.isChecked = hideNonVeg()
+        }
+        binding.rbR1NonVeg.setOnClickListener {
+            binding.rbR1NonVeg.isChecked = showNonVeg()
+        }
+    }
+
+    private fun hideNonVeg(): Boolean {
+        binding.cbOnionRings.visibility = View.GONE
+        binding.cbEgg.visibility =View.GONE
+        binding.cbChicken.visibility =View.GONE
+        binding.cbMutton.visibility =View.GONE
+        return true
+    }
+
+    private fun showNonVeg(): Boolean {
+        binding.cbOnionRings.visibility = View.VISIBLE
+        binding.cbEgg.visibility =View.VISIBLE
+        binding.cbChicken.visibility =View.VISIBLE
+        binding.cbMutton.visibility =View.VISIBLE
+        return true
     }
 }
