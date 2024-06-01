f5cdf87e38467e2c1362be3dd1a4ec6f93771faa
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-02 02:18:29 +0530

    normal Toast added in Normal Button

--- CustomToast/app/src/main/java/com/example/customtoast/MainActivity.kt
+++ CustomToast/app/src/main/java/com/example/customtoast/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.customtoast
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.customtoast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val f = false
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        if (f) {
+            setContentView(R.layout.activity_main)
+        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnNormal.setOnClickListener {
+            Toast.makeText(this, "Its a Normal Toast!", Toast.LENGTH_SHORT).show()
         }
     }
 }
