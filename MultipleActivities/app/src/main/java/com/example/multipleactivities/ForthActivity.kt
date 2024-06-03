29985fa07691596773725133cb554b6fbf4d5821
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-03 21:28:00 +0530

    All Three buttons are set in ForthActivity.kt for Activity Four

--- MultipleActivities/app/src/main/java/com/example/multipleactivities/ForthActivity.kt
+++ MultipleActivities/app/src/main/java/com/example/multipleactivities/ForthActivity.kt
@@ -1,6 +1,8 @@
 package com.example.multipleactivities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,26 @@ class ForthActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.btnA4toA3).setOnClickListener {
+            finish()
+        }
+
+        findViewById<Button>(R.id.btnA4toA1).setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(it)
+                finish()
+            }
+        }
+
+        findViewById<Button>(R.id.btnA4toA2).setOnClickListener {
+            Intent(this, SecondActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(it)
+                finish()
+            }
         }
     }
 }
