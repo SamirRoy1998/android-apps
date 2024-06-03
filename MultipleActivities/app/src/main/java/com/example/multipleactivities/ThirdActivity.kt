883f489aa31947221503c9b5b20ebbc2c26ea80d
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-03 18:12:50 +0530

    First page button set to MainActivity and Second page button set to Previous SecondActivity

--- MultipleActivities/app/src/main/java/com/example/multipleactivities/ThirdActivity.kt
+++ MultipleActivities/app/src/main/java/com/example/multipleactivities/ThirdActivity.kt
@@ -1,6 +1,8 @@
 package com.example.multipleactivities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,5 +11,17 @@ class ThirdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_third)
+
+        findViewById<Button>(R.id.btnA3toA1).setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        findViewById<Button>(R.id.btnA3toA2).setOnClickListener {
+            Intent(this, SecondActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
