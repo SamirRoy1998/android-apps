0eac0830131d6b503b453cbfd61b262247005bb0
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-03 17:57:05 +0530

    Second page button set to next SecondActivity

--- MultipleActivities/app/src/main/java/com/example/multipleactivities/MainActivity.kt
+++ MultipleActivities/app/src/main/java/com/example/multipleactivities/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.multipleactivities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.btnA1toA2).setOnClickListener {
+            Intent(this, SecondActivity::class.java).also {
+                startActivity(it)
+            }
         }
     }
 }
