69f8b0d9c43bb9986765abf37491451e9107d317
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-04 02:54:52 +0530

    dataBinding set in MainActivity.kt

--- NewStart/app/src/main/java/com/example/newstart/MainActivity.kt
+++ NewStart/app/src/main/java/com/example/newstart/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.newstart
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.newstart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.firstActivity = this
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
