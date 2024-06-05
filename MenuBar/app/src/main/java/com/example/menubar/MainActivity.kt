a7ecec0d2e04b9de7c7934cf2a4ff54b07605676
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-06 00:45:19 +0530

    set a override method to infate customize action bar

--- MenuBar/app/src/main/java/com/example/menubar/MainActivity.kt
+++ MenuBar/app/src/main/java/com/example/menubar/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.menubar
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,10 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_menu_bar, menu)
+        return true
+    }
+
 }
