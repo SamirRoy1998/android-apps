1996538aa504c8e1ba75a83ec9cc4635ea2dd1c1
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-06 19:29:22 +0530

    first Dialog box created and set on FirstButton

--- AlertDialogBox/app/src/main/java/com/example/alertdialogbox/MainActivity.kt
+++ AlertDialogBox/app/src/main/java/com/example/alertdialogbox/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.alertdialogbox
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -20,6 +22,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val addContactDialogBox = AlertDialog.Builder(this)
+            .setTitle("Add Contact")
+            .setMessage("Do you want to add this person to your contacts list?")
+            .setIcon(R.drawable.ic_add_contact)
+            .setPositiveButton("Yes") { _, _ ->
+                Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("No") { _, _ ->
+                Toast.makeText(this, "No", Toast.LENGTH_SHORT).show()
+            }.create()
+
+        binding.btnFirstDailog.setOnClickListener {
+            addContactDialogBox.show()
         }
     }
 }
