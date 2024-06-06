10a868d8738a282c52cd5c6aac69abbfe6e7be45
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-06 19:02:34 +0530

    bataBinding set Kotlin file to xml file

--- AlertDialogBox/app/src/main/java/com/example/alertdialogbox/MainActivity.kt
+++ AlertDialogBox/app/src/main/java/com/example/alertdialogbox/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.alertdialogbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.acFirst = this
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
