b3d316faa31d5439fbabaf5ded529619606609e9
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-07 02:56:52 +0530

    all button function set with dataBinding

--- MultipleActivities/app/src/main/java/com/example/multipleactivities/FifthActivity.kt
+++ MultipleActivities/app/src/main/java/com/example/multipleactivities/FifthActivity.kt
@@ -1,20 +1,50 @@
 package com.example.multipleactivities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.multipleactivities.databinding.ActivityFifthBinding
 
 class FifthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFifthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_fifth)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_fifth)
+        binding.fifthPage = this
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clsFifthActivity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnA5toA4.setOnClickListener {
+            finish()
+        }
+
+        binding.btnA5toA3.setOnClickListener {
+            Intent(this, ThirdActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(it)
+            }
+        }
+
+        binding.btnA5toA2.setOnClickListener {
+            Intent(this, SecondActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(it)
+            }
+        }
+
+        binding.btnA5toA1.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(it)
+            }
         }
     }
 }
