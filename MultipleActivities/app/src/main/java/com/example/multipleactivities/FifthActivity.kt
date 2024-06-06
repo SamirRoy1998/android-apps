f23d93fcc045284772b3abc73116a8b8f1df8fa9
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-07 02:37:08 +0530

    bug fix

--- MultipleActivities/app/src/main/java/com/example/multipleactivities/FifthActivity.kt
+++ MultipleActivities/app/src/main/java/com/example/multipleactivities/FifthActivity.kt
@@ -11,7 +11,7 @@ class FifthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_fifth)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clsFifthActivity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
