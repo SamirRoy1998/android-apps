4527fc7d0a3add769b24c69018c306cae32d740a
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-08 21:48:30 +0530

    page 3 added

---
+++ MyBook/app/src/main/java/com/example/mybook/Page3Activity.kt
@@ -0,0 +1,24 @@
+package com.example.mybook
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.mybook.databinding.ActivityPage3Binding
+
+class Page3Activity : AppCompatActivity() {
+    private lateinit var b: ActivityPage3Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        b = DataBindingUtil.setContentView(this, R.layout.activity_page3)
+        b.page3 = this
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clPage3)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
