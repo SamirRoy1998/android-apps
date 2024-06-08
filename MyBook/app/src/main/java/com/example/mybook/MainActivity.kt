f8fa48563156e7707ddce9371c2ad9257bb884ef
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-08 23:01:48 +0530

    Cover page option button on click AlertDialog Box funtion set

--- MyBook/app/src/main/java/com/example/mybook/MainActivity.kt
+++ MyBook/app/src/main/java/com/example/mybook/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mybook
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,6 +20,43 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val option = arrayOf("Page One", "Page Two", "Page Three", "Page Four", "Page Five", "Close App")
+        val singleChoiceDialogBox = AlertDialog.Builder(this)
+            .setTitle("Choose one of this option ")
+            .setSingleChoiceItems(option, 0) { _, _ ->
+            }
+            .setPositiveButton("Go") { dialog, _ ->
+                val selectedPosition = (dialog as AlertDialog).listView.checkedItemPosition
+                if (selectedPosition == 0) {
+
+                }
+                dialog.dismiss()
+                if (selectedPosition == 1) {
+
+                }
+                dialog.dismiss()
+                if (selectedPosition == 2) {
+
+                }
+                dialog.dismiss()
+                if (selectedPosition == 3) {
+
+                }
+                dialog.dismiss()
+                if (selectedPosition == 4) {
+
+                }
+                dialog.dismiss()
+                if (selectedPosition == 5) {
+                    finish()
+                }
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel"){_,_->}
+
+        b.btnOptionCover.setOnClickListener {
+            singleChoiceDialogBox.show()
         }
     }
 }
