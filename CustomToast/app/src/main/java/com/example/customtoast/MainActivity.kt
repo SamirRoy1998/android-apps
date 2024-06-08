e8da8431ff90c8eed897bd009f57fc527923d7be
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-08 19:10:47 +0530

    unused import removed

--- CustomToast/app/src/main/java/com/example/customtoast/MainActivity.kt
+++ CustomToast/app/src/main/java/com/example/customtoast/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.customtoast
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
