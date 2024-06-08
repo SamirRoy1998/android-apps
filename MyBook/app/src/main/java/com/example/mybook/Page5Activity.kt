4744d018f94fc2a047fb00f90e60a406abf4543f
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-08 22:35:25 +0530

    page 5 added

---
+++ MyBook/app/src/main/java/com/example/mybook/Page5Activity.kt
@@ -0,0 +1,24 @@
+package com.example.mybook
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.mybook.databinding.ActivityPage5Binding
+
+class Page5Activity : AppCompatActivity() {
+    private lateinit var b: ActivityPage5Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        b = DataBindingUtil.setContentView(this, R.layout.activity_page5)
+        b.page5 = this
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clPage5)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
