fd5b9fc6f61017faf8cc5149da61196c7dea97b9
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-08 21:41:42 +0530

    page 2 added

---
+++ MyBook/app/src/main/java/com/example/mybook/Page2Activity.kt
@@ -0,0 +1,24 @@
+package com.example.mybook
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.mybook.databinding.ActivityPage2Binding
+
+class Page2Activity : AppCompatActivity() {
+    private lateinit var b: ActivityPage2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        b = DataBindingUtil.setContentView(this, R.layout.activity_page2)
+        b.page2 = this
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clPage2)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
