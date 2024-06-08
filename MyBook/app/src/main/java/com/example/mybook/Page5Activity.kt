65be8f22f10e6ca9b4034eafc3085e82977d6024
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-09 02:38:28 +0530

    pageFive done

--- MyBook/app/src/main/java/com/example/mybook/Page5Activity.kt
+++ MyBook/app/src/main/java/com/example/mybook/Page5Activity.kt
@@ -1,7 +1,9 @@
 package com.example.mybook
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,6 +21,54 @@ class Page5Activity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val option =
+            arrayOf("Page One", "Page Two", "Page Three", "Page Four", "Close Book", "Close App")
+        val optionAlertDialog = AlertDialog.Builder(this)
+            .setTitle("Choose one of this option")
+            .setSingleChoiceItems(option, 4, null)
+            .setPositiveButton("Go") { dialog, _ ->
+                val selectPosition = (dialog as AlertDialog).listView.checkedItemPosition
+                when (selectPosition) {
+                    0 -> backNavigate(Page1Activity::class.java)
+                    1 -> backNavigate(Page2Activity::class.java)
+                    2 -> backNavigate(Page3Activity::class.java)
+                    3 -> backNavigate(Page4Activity::class.java)
+                    4 -> closeBook()
+                    5 -> finishAffinity()
+                }
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+
+        b.btnOptionPage5.setOnClickListener {
+            optionAlertDialog.show()
+        }
+    }
+
+    private fun backNavigate(x: Class<*>) {
+        Intent(this, x).also {
+            it.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(it)
+            finish()
+        }
+    }
+
+    private fun closeBook() {
+        Intent(this, MainActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+            finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Intent(this, Page4Activity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+            finish()
         }
     }
 }
