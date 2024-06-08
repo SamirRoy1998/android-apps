c16836ba3063c4864f31fa6d457428598b110163
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-09 01:50:42 +0530

    pageTwo Option button set

--- MyBook/app/src/main/java/com/example/mybook/Page2Activity.kt
+++ MyBook/app/src/main/java/com/example/mybook/Page2Activity.kt
@@ -1,7 +1,9 @@
 package com.example.mybook
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,6 +21,51 @@ class Page2Activity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val option =
+            arrayOf("Page One", "Page Three", "Page Four", "Page Five", "Close Book", "Close App")
+        val singleChoiceAlertDialogBox = AlertDialog.Builder(this)
+            .setTitle("Choose one of this option")
+            .setSingleChoiceItems(option, 1, null)
+            .setPositiveButton("Go") { dialog, _ ->
+                val selectPosition = (dialog as AlertDialog).listView.checkedItemPosition
+                when (selectPosition) {
+                    0 -> backNavigate(Page1Activity::class.java)
+                    1 -> navigate(Page3Activity::class.java)
+                    2 -> navigate(Page4Activity::class.java)
+                    3 -> navigate(Page5Activity::class.java)
+                    4 -> closeBook()
+                    5 -> finishAffinity()
+                }
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+
+        b.btnOptionPage2.setOnClickListener {
+            singleChoiceAlertDialogBox.show()
+        }
+    }
+
+    private fun navigate(activityClass: Class<*>) {
+        Intent(this, activityClass).also {
+            startActivity(it)
+        }
+    }
+
+    private fun backNavigate(activityClass: Class<*>) {
+        Intent(this, activityClass).also {
+            it.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(it)
+            finish()
+        }
+    }
+
+    private fun closeBook() {
+        Intent(this, MainActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+            finish()
         }
     }
 }
