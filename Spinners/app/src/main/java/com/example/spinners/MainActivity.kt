520c88e69c9271611eb1e3a116a5f912047de0d0
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-09 21:04:55 +0530

    dataBinding set in main layout

--- Spinners/app/src/main/java/com/example/spinners/MainActivity.kt
+++ Spinners/app/src/main/java/com/example/spinners/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.spinners.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        b = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        b.lMain = this
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
