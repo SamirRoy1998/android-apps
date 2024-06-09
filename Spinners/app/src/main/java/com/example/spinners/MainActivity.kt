0b98a4376c0d791c9b77692069b565fa6f3f0e0c
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-09 22:46:13 +0530

    unused import remove

--- Spinners/app/src/main/java/com/example/spinners/MainActivity.kt
+++ Spinners/app/src/main/java/com/example/spinners/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.spinners
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
