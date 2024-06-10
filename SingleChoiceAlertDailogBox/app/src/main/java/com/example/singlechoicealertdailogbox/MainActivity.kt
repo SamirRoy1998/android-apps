4dd13481b1556604e9bfb00815e68cdedeaab7c4
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-10 17:39:13 +0530

    single choice AlertDialog set in option button

--- SingleChoiceAlertDailogBox/app/src/main/java/com/example/singlechoicealertdailogbox/MainActivity.kt
+++ SingleChoiceAlertDailogBox/app/src/main/java/com/example/singlechoicealertdailogbox/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.singlechoicealertdailogbox
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,6 +21,28 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val color = arrayOf("Red", "Blue", "Green", "Yellow", "Pink")
+        val singleChoiceAlertDialog = AlertDialog.Builder(this)
+            .setTitle("Choose one of this option")
+            .setSingleChoiceItems(color, 1, null)
+            .setPositiveButton("Accept") { dialog, _ ->
+                val selectedPosition = (dialog as AlertDialog).listView.checkedItemPosition
+                when (selectedPosition) {
+                    0 -> b.txtResult.text = "Red"
+                    1 -> b.txtResult.text = "Blue"
+                    2 -> b.txtResult.text = "Green"
+                    3 -> b.txtResult.text = "Yellow"
+                    4 -> b.txtResult.text = "Pink"
+                }
+                b.txtResult.visibility = View.VISIBLE
+                dialog.dismiss()
+            }
+            .setNegativeButton("Decline", null)
+            .create()
+
+        b.btnOption.setOnClickListener {
+            singleChoiceAlertDialog.show()
         }
     }
 }
