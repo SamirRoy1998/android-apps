a86bd7bd905dc6a7ca8137ed483b950e543b8deb
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-12 04:18:50 +0530

    auto complete text view set

--- AutoCompleteTextView/app/src/main/java/com/example/autocompletetextview/MainActivity.kt
+++ AutoCompleteTextView/app/src/main/java/com/example/autocompletetextview/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.autocompletetextview
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,5 +21,65 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val language =
+            arrayOf(
+                "English Language",
+                "Bengali Language",
+                "Assamese Language",
+                "Bodo Language",
+                "Nepali Language",
+                "Marathi Language",
+                "Gujarati Language",
+                "Hindi Language",
+                "Kannada Language",
+                "Kashmiri Language",
+                "Konkani Language",
+                "Malayalam Language",
+                "Manipuri Language",
+                "Oriya Language",
+                "Punjabi Language",
+                "Sanskrit Language",
+                "Sindhi Language",
+                "Tamil Language",
+                "Telugu Language",
+                "Urdu Language",
+                "French Language",
+                "German Language",
+                "Spanish Language",
+                "Italian Language",
+                "Portuguese Language",
+                "Russian Language",
+                "Chinese Language",
+                "Japanese Language",
+                "Korean Language",
+                "Vietnamese Language",
+                "Thai Language",
+                "Swahili Language",
+                "Arabic Language",
+                "Turkish Language",
+                "Persian Language",
+                "Greek Language",
+                "Dutch Language",
+                "Hungarian Language",
+                "Polish Language",
+                "Romanian Language",
+                "Czech Language",
+                "Slovak Language",
+                "Bulgarian Language",
+                "Serbian Language",
+                "Croatian Language",
+                "Ukrainian Language",
+                "Hebrew Language",
+                "Finnish Language",
+                "Norwegian Language",
+                "Swedish Language"
+            )
+        val customAdapter = ArrayAdapter(
+            this,
+            com.google.android.material.R.layout.m3_auto_complete_simple_item,
+            language
+        )
+        b.actvSearch.setAdapter(customAdapter)
+        b.actvSearch.threshold = 4
     }
 }
