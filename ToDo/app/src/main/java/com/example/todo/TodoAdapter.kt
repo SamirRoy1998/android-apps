ae8a91b420090a65375a7831cd360d0fe4818da1
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-13 01:56:41 +0530

    override function added

--- ToDo/app/src/main/java/com/example/todo/TodoAdapter.kt
+++ ToDo/app/src/main/java/com/example/todo/TodoAdapter.kt
@@ -1,9 +1,22 @@
 package com.example.todo
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdapter(var todos: List<Todo>) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
     inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
