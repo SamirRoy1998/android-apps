b44d730ca6d61d9c30fcd15bb45bb694af4dc6c3
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-13 02:04:40 +0530

    onCreateViewHolder set

--- ToDo/app/src/main/java/com/example/todo/TodoAdapter.kt
+++ ToDo/app/src/main/java/com/example/todo/TodoAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.todo
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -9,7 +10,8 @@ class TodoAdapter(var todos: List<Todo>) : RecyclerView.Adapter<TodoAdapter.Todo
     inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.todo_items, parent, false)
+        return TodoViewHolder(view)
     }
 
     override fun getItemCount(): Int {
