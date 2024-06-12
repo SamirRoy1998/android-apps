704d57ab38e3f372320498fdf246736719b4bb85
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-13 01:53:53 +0530

    almost Adapter ready need to implement override function of RecyclerView.Adapter

--- ToDo/app/src/main/java/com/example/todo/TodoAdapter.kt
+++ ToDo/app/src/main/java/com/example/todo/TodoAdapter.kt
@@ -3,7 +3,7 @@ package com.example.todo
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class TodoAdapter {
+class TodoAdapter(var todos: List<Todo>) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
     inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
