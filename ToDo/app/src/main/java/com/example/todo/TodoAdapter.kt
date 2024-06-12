8506173eec822924e18a7c5f742f6fe013a24cb2
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-13 01:48:40 +0530

    inner ViewHolder class added in custom Adapter

--- ToDo/app/src/main/java/com/example/todo/TodoAdapter.kt
+++ ToDo/app/src/main/java/com/example/todo/TodoAdapter.kt
@@ -1,4 +1,9 @@
 package com.example.todo
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
 class TodoAdapter {
+
+    inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
