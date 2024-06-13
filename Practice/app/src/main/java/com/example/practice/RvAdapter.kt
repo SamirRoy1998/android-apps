ba4bb6f55881f066e4a10597d21ccf0da0fe1b5a
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-13 19:34:22 +0530

    <Pr1> Adapter Ready

--- Practice/app/src/main/java/com/example/practice/RvAdapter.kt
+++ Practice/app/src/main/java/com/example/practice/RvAdapter.kt
@@ -1,4 +1,28 @@
 package com.example.practice
 
-class RvAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class RvAdapter(val list: List<Data>) : RecyclerView.Adapter<RvAdapter.RvViewHolder>() {
+    inner class RvViewHolder(view: View) : ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.rv_layout, parent, false)
+        return RvViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
+        holder.itemView.apply {
+            findViewById<TextView>(R.id.txtRvTitle).text = list[position].name
+            findViewById<TextView>(R.id.txtRvNumber).text = list[position].number
+        }
+    }
 }
