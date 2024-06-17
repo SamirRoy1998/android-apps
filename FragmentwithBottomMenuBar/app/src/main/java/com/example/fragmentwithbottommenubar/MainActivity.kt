106718c296770de84cc635804e675a142b5503d9
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-18 00:41:03 +0530

    dataBinding enable and set

--- FragmentwithBottomMenuBar/app/src/main/java/com/example/fragmentwithbottommenubar/MainActivity.kt
+++ FragmentwithBottomMenuBar/app/src/main/java/com/example/fragmentwithbottommenubar/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.fragmentwithbottommenubar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        b = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        b.mainLayout = this
+        ViewCompat.setOnApplyWindowInsetsListener(b.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
