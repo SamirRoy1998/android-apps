3701c3c9387a9546fce6c5ed4f2448296389220f
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-18 02:55:29 +0530

    image list ready

--- ViewPager/app/src/main/java/com/example/viewpager/MainActivity.kt
+++ ViewPager/app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -10,6 +10,28 @@ import com.example.viewpager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var b: ActivityMainBinding
+    private val images = listOf(
+        R.drawable.man1,
+        R.drawable.woman1,
+        R.drawable.man2,
+        R.drawable.woman2,
+        R.drawable.man3,
+        R.drawable.woman3,
+        R.drawable.man4,
+        R.drawable.woman4,
+        R.drawable.man5,
+        R.drawable.woman5,
+        R.drawable.man6,
+        R.drawable.woman6,
+        R.drawable.man7,
+        R.drawable.woman7,
+        R.drawable.man8,
+        R.drawable.woman8,
+        R.drawable.man9,
+        R.drawable.woman9,
+        R.drawable.man10,
+        R.drawable.woman10,
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
