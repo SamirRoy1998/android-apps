30533413cce4b85a1af961231942cfb7f9cbf6a7
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-19 02:18:07 +0530

    Custom Adapter done using dataBinding

--- ViewPager/app/src/main/java/com/example/viewpager/ViewPagerAdapter.kt
+++ ViewPager/app/src/main/java/com/example/viewpager/ViewPagerAdapter.kt
@@ -1,21 +1,17 @@
 package com.example.viewpager
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.viewpager.databinding.ViewPagerItemBinding
 
 class ViewPagerAdapter(private val list: List<Int>) :
     RecyclerView.Adapter<ViewPagerAdapter.VpViewHolder>() {
-    inner class VpViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val image: ImageView = itemView.findViewById(R.id.ivImage)
-    }
+    inner class VpViewHolder(val b: ViewPagerItemBinding) : RecyclerView.ViewHolder(b.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VpViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.view_pager_item, parent, false)
-        return VpViewHolder(view)
+        val b = ViewPagerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return VpViewHolder(b)
     }
 
     override fun getItemCount(): Int {
@@ -24,6 +20,6 @@ class ViewPagerAdapter(private val list: List<Int>) :
 
     override fun onBindViewHolder(holder: VpViewHolder, position: Int) {
         val item = list[position]
-        holder.image.setImageResource(item)
+        holder.b.ivImage.setImageResource(item)
     }
 }
