73f019e70484414fdf2a14872e3a8a1c28019ef8
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-18 19:13:49 +0530

    <Pr3> FirstFragment next button listener set

--- Practice/app/src/main/java/com/example/practice/FirstFragment.kt
+++ Practice/app/src/main/java/com/example/practice/FirstFragment.kt
@@ -1,5 +1,24 @@
 package com.example.practice
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
-class FirstFragment :Fragment(R.layout.fragment_first)
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+        view.findViewById<Button>(R.id.btnNext).setOnClickListener {
+            val frag = SecondFragment()
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.flFragment, frag).addToBackStack(null).commit()
+        }
+        return view
+    }
+}
