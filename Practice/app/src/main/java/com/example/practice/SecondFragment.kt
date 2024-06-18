9e561005d261c979095f9dd7b2231a17427b4726
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-18 21:02:52 +0530

    <Pr3> Second Fragment binding set and btnBack listener set

--- Practice/app/src/main/java/com/example/practice/SecondFragment.kt
+++ Practice/app/src/main/java/com/example/practice/SecondFragment.kt
@@ -1,5 +1,27 @@
 package com.example.practice
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.practice.databinding.FragmentSecondBinding
 
-class SecondFragment :Fragment(R.layout.fragment_second)
+class SecondFragment : Fragment() {
+    private lateinit var b: FragmentSecondBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        b = FragmentSecondBinding.inflate(inflater, container, false)
+        return b.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        b.btnBack.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+    }
+}
