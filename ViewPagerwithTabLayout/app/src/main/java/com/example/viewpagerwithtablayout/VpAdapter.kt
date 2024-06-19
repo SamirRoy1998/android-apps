81c41cf67e59c8d34311de35c1f57598a0751c8b
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-19 16:41:05 +0530

    viewpager adapter created

--- ViewPagerwithTabLayout/app/src/main/java/com/example/viewpagerwithtablayout/VpAdapter.kt
+++ ViewPagerwithTabLayout/app/src/main/java/com/example/viewpagerwithtablayout/VpAdapter.kt
@@ -1,4 +1,26 @@
 package com.example.viewpagerwithtablayout
 
-class VpAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.viewpagerwithtablayout.databinding.ViewpagerItemsBinding
+
+class VpAdapter(private val list: List<Int>) : RecyclerView.Adapter<VpAdapter.VpViewHolder>() {
+    inner class VpViewHolder(val binding: ViewpagerItemsBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VpViewHolder {
+        val bindingView =
+            ViewpagerItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return VpViewHolder(bindingView)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: VpViewHolder, position: Int) {
+        val image = list[position]
+        holder.binding.ivVpImage.setImageResource(image)
+    }
 }
