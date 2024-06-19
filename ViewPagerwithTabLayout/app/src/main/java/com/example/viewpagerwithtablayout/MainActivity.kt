3123342a138c56f3c7855e967d97a351ed55a9e6
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-19 18:32:44 +0530

    tablayout tabSelected Listener added

--- ViewPagerwithTabLayout/app/src/main/java/com/example/viewpagerwithtablayout/MainActivity.kt
+++ ViewPagerwithTabLayout/app/src/main/java/com/example/viewpagerwithtablayout/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.viewpagerwithtablayout
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.example.viewpagerwithtablayout.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +27,22 @@ class MainActivity : AppCompatActivity() {
         val viewPager = binding.viewPager
         val tabLayout = binding.tabLayout
         viewPager.adapter = VpAdapter(storage.images)
+
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = "${position + 1}"
         }.attach()
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity,"you selected ${tab?.text}",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?){
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity,"you reselected ${tab?.text}",Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
