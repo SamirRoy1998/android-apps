e373c25ca3346d0238885e6779e405873973248b
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-19 17:09:52 +0530

    storage class data added for use

--- ViewPagerwithTabLayout/app/src/main/java/com/example/viewpagerwithtablayout/Storage.kt
+++ ViewPagerwithTabLayout/app/src/main/java/com/example/viewpagerwithtablayout/Storage.kt
@@ -1,4 +1,26 @@
 package com.example.viewpagerwithtablayout
 
 class Storage {
+    val images = listOf(
+        R.drawable.man1,
+        R.drawable.woman1,
+        R.drawable.man2,
+        R.drawable.woman2,
+        R.drawable.man3,
+        R.drawable.woman3,
+        R.drawable.man4,
+        R.drawable.woman4,
+        R.drawable.man5,
+        R.drawable.woman5,
+        R.drawable.man6,
+        R.drawable.woman6,
+        R.drawable.man7,
+        R.drawable.woman7,
+        R.drawable.man8,
+        R.drawable.woman8,
+        R.drawable.man9,
+        R.drawable.woman9,
+        R.drawable.man10,
+        R.drawable.woman10
+    )
 }
