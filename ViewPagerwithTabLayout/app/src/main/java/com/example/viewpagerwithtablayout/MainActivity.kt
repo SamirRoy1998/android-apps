698f70471d322b9e9ec60bb0d71e1a46b87b5ae7
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-19 17:11:16 +0530

    Adapter set in MainActivity

--- ViewPagerwithTabLayout/app/src/main/java/com/example/viewpagerwithtablayout/MainActivity.kt
+++ ViewPagerwithTabLayout/app/src/main/java/com/example/viewpagerwithtablayout/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.viewpagerwithtablayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val storage = Storage()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +21,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.viewPager.adapter = VpAdapter(storage.images)
     }
 }
