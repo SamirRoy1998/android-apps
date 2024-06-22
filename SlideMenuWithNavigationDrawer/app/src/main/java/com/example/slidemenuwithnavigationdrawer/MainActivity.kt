044728ea630a883cc48aebb6c3199ff31f921ee5
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-23 01:50:06 +0530

    dataBinding enable and set

--- SlideMenuWithNavigationDrawer/app/src/main/java/com/example/slidemenuwithnavigationdrawer/MainActivity.kt
+++ SlideMenuWithNavigationDrawer/app/src/main/java/com/example/slidemenuwithnavigationdrawer/MainActivity.kt
@@ -2,44 +2,41 @@ package com.example.slidemenuwithnavigationdrawer
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
+import androidx.databinding.DataBindingUtil
+import com.example.slidemenuwithnavigationdrawer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout)) { v, insets ->
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.drawerLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
 
         toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
+            this, binding.drawerLayout,
             R.string.open,
             R.string.close
         )
-        drawerLayout.addDrawerListener(toggle)
+        binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val navView: NavigationView = findViewById(R.id.navView)
-        navView.setNavigationItemSelectedListener {
+        binding.navView.setNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.miTitle1 -> {
-                    findViewById<TextView>(R.id.tvText).text = "Title 1"
+                    binding.tvText.text = "Title 1"
                     Toast.makeText(applicationContext, "Title 1", Toast.LENGTH_SHORT)
                     .show()
                 }
