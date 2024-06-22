34ec4ff6e7a408f664727a4c77f61269d0e8dc21
SamirRoy1998 <dev.sam693@gmail.com>
2024-06-23 01:13:14 +0530

    drawer toggle function set

--- SlideMenuWithNavigationDrawer/app/src/main/java/com/example/slidemenuwithnavigationdrawer/MainActivity.kt
+++ SlideMenuWithNavigationDrawer/app/src/main/java/com/example/slidemenuwithnavigationdrawer/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.slidemenuwithnavigationdrawer
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.open,
+            R.string.close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
